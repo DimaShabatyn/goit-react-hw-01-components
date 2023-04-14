f6de6510ea8092f4670e1d5c56ae33cfbc40278d
DimaShabatyn <dmitriyshabatyn@gmail.com>
2023-04-15 01:05:51 +0300

    add styles

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -6,21 +6,21 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
     <div className={css.profile}>
       <div className={css.description}>
         <img src={avatar} alt="User avatar" className="avatar" />
-        <p>{username}</p>
+        <p className={css.name}>{username}</p>
         <p className="tag">@{tag}</p>
         <p className="location">{location}</p>
       </div>
 
       <ul className={css.stats}>
-        <li>
+        <li className={css.item}>
           <span className="label">Followers </span>
           <span className="quantity">{stats.followers}</span>
         </li>
-        <li>
+        <li className={css.item}>
           <span className="label">Views </span>
           <span className="quantity">{stats.views}</span>
         </li>
-        <li>
+        <li className={css.item}>
           <span className="label">Likes </span>
           <span className="quantity">{stats.likes}</span>
         </li>
